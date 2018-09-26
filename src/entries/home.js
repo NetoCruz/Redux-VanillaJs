b39a4338f886ffd689c872eba5d81327d0fdfef7
NetoCruz <galaxy.net0710@gmail.com>
2018-09-26 15:10:23 -0500

    Datos Normalizados

--- src/entries/home.js
+++ src/entries/home.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { hydrate } from 'react-dom';
 import Home from '../pages/containers/home';
 // import Playlist from './src/playlist/components/playlist';
-import data from '../api.json';
+// import data from '../api.json';
 // console.log('Hola mundo!' )
+import data from '../schemas/index.js';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from '../reducers/data.js'
 
 const initialState={
 	data:{
-		...data
+		// ...data
+		entities:data.entities,
+		categories: data.result.categories,
 		},
 	search: []
 }
